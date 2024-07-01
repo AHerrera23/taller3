845655b253d4741b67dd570f11b622daa39cf89b
AHerrera23 <herreraabel892@gmail.com>
2024-07-01 02:00:16 -0300

    avances de pagina

--- js/main.js
+++ js/main.js
@@ -3,13 +3,11 @@ let error = document.getElementById("error");
 let card = document.getElementById("card");
 let cardPaciente=document.getElementById("cardPaciente")
 let nombrePaciente=document.getElementById("paciente")
-let particulas = document.getElementById("contenedorParticulas")
 const test = ()=>{
 let user = document.getElementById("nombreUsuario").value
 console.log(user)
 console.log(typeof user)
 card.style.display="none"
 cardPaciente.style.display="flex"
-nombrePaciente.innerHTML="Bienvenido " + user
-particulas.classList.add("off")
+nombrePaciente.innerHTML="Bienvenido " + "<span>"+user+"</span>"
 }
