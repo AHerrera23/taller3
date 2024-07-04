d38665ee5724433774ea86405272e3f26ff8d140
JuliaGimenez <juligimenez18@hotmail.com>
2024-07-04 03:04:41 -0300

    ultimos commit

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ let error = document.getElementById("error")
 let card = document.getElementById("card")
 let cardPaciente=document.getElementById("cardPaciente")
 let nombrePaciente=document.getElementById("paciente")
+let dosis=document.getElementById('dosis')
+let monoxido=document.getElementById('monoxido')
+let saturacion=document.getElementById('saturacion')
+let valor
+let valor2
+let valor3
 
-const randomInt=(max)=>{
-    return Math.floor(Math.random()*max)
+const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
+
 
 const test = ()=>{
 let user = document.getElementById("nombreUsuario").value
@@ -15,5 +22,8 @@ console.log(typeof user)
 card.style.display="none"
 cardPaciente.style.display="flex"
 nombrePaciente.innerHTML="Buenas tardes:" + " <span>"+user+"</span> <br>¿Como te sentis el dia de hoy?"
+dosis.innerHTML='Porcentaje de dosis: <br> En sangre: ' +' <span>'+randomInt(0,5)+'</span>' +'% <br>'+'En dispositivo: ' +' <span>'+randomInt(98,100)+'</span>'+'%'
+monoxido.innerHTML='Porcentaje de monoxido en sangre: ' + '<span>'+randomInt(70,90)+'</span>' +'%'
+saturacion.innerHTML='Porcentaje de saturacion de oxigeno en sangre: ' + '<span>'+randomInt(40,80)+'</span>' +'%'
 
 }
